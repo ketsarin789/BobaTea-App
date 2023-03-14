2932224e47b25408d606a3a3c4467e65eadb6d1e
Ketsarin Kayak <ketsarinkayak@Ketsarins-MacBook-Pro.local>
2023-03-14 06:24:55 -0700

    Fixed Favorite

--- pages/Favorites.js
+++ pages/Favorites.js
@@ -4,17 +4,29 @@ import { Avatar, ListItem} from 'react-native-elements';
 import {STORE} from '../data/items';
 
 export const FavoriteScreen = () => {
-
-    const {name,image,id} = STORE;
+   
+    
 
     return (
         <View style={{paddingTop: 100, alignSelf: 'center'}}>
             <Text style={{fontSize: 50}}>Favorite</Text>
-            <Avatar rounded source={{ image }} />
-            <ListItem.Content>
-                    <ListItem.Title>{name}</ListItem.Title>
-                    
-                </ListItem.Content>
+            <View >
+                {STORE.map((favorite) => (
+                    <View style={{flexDirection: 'row', padding: 20}}>
+                    <Avatar rounded source={favorite.image} style={{width: 50, height: 50}}/>
+                    <Text style={{marginLeft: 30, paddingTop: 15, fontSize: 20}}>{favorite.name}</Text>
+                    </View>
+                ))}
+               
+            </View>
+            
+            
         </View>
     )
 }
+
+{/* <Avatar rounded source={{ image }} />
+<ListItem.Content>
+        <ListItem.Title>{name}</ListItem.Title>
+        
+</ListItem.Content> */}
